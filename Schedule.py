e9a66d98033247cdecfa937fe4e21830f55a40b5
dominicp6 <dom.phillips97@gmail.com>
2023-01-22 20:21:07 +0000

    printing events in schedule

--- Schedule.py
+++ Schedule.py
@@ -1,7 +1,8 @@
 import bisect
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from Task import Task
+from Event import Event
 from utils import get_input, bcolors
 
 
@@ -15,24 +16,41 @@ class Schedule(object):
     def __str__(self):
         dividing_bar = False
         string = "=======================================================================\n"
-        for (time, task) in self.schedule:
+        for (time, obligation) in self.schedule:
             if time[0] > 12 and not dividing_bar:
                 string += "-----------------------------------------------------------------------\n"
                 dividing_bar = True
-            days_remaining = (task.due_date - self.date).days + 1
-            if days_remaining < 0:
-                string += f"{task.name} at {time} {bcolors.RED}[Overdue]{bcolors.ENDC}\n"
-            elif days_remaining <=2:
-                if days_remaining == 0:
-                    string += f"{task.name} at {time} {bcolors.YELLOW}[Due Today]{bcolors.ENDC}\n"
-                elif days_remaining == 1:
-                    string += f"{task.name} at {time} {bcolors.YELLOW}[Due Tomorrow]{bcolors.ENDC}\n"
-                elif days_remaining == 2:
-                    string += f"{task.name} at {time} {bcolors.YELLOW}[Due {task.due_date.strftime('%A')}]{bcolors.ENDC}\n"
+            
+            if isinstance(obligation, Task):
+                string = self._add_task_string(obligation, time, string)
+            elif isinstance(obligation, Event):
+                string = self._add_event_string(obligation, time, string)
             else:
-                string += f"{task.name} at {time}\n"
+                raise TypeError("Obligation must be a Task or Event")
+                
         string += "=======================================================================\n"
         string += "\n"
+
+        return string
+
+    def _add_task_string(self, task: Task, time: tuple[float, float], string: str):
+        days_remaining = (task.due_date - self.date).days + 1
+        if days_remaining < 0:
+            string += f"{task.name} at {time} {bcolors.RED}[Overdue]{bcolors.ENDC}\n"
+        elif days_remaining <=2:
+            if days_remaining == 0:
+                string += f"{task.name} at {time} {bcolors.YELLOW}[Due Today]{bcolors.ENDC}\n"
+            elif days_remaining == 1:
+                string += f"{task.name} at {time} {bcolors.YELLOW}[Due Tomorrow]{bcolors.ENDC}\n"
+            elif days_remaining == 2:
+                string += f"{task.name} at {time} {bcolors.YELLOW}[Due {task.due_date.strftime('%A')}]{bcolors.ENDC}\n"
+        else:
+            string += f"{task.name} at {time}\n"
+
+        return string
+
+    def _add_event_string(self, event: Event, time: tuple[float, float], string: str):
+        string += f"{bcolors.GREEN}{event.name} at {time}{bcolors.GREEN}\n"
 
         return string
 
@@ -64,8 +82,9 @@ class Schedule(object):
             self.update_remaining_hours(time)
             bisect.insort(self.schedule, (time, task))
 
-    def add_event(self, event, time: tuple[float, float]):
+    def add_event(self, event: Event, time: tuple[float, float]):
         if self.is_available(time):
+            print(f"Adding {event.name} at {time} on {self.date.strftime('%A, %B %d, %Y')}")
             self.update_remaining_hours(time)
             bisect.insort(self.schedule, (time, event))
 
